e1a1b870ca0bccfb0842c1c95e9cdc8d54b1478c
Jay Phelps <hello@jayphelps.com>
2021-10-06 12:17:20 -0700

    Add 'this' alternate

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export default typeof globalThis === "object" ? globalThis
              : typeof global     === "object" ? global
              : typeof window     === "object" ? window
              : typeof self       === "object" ? self
+             : typeof this       === "object" ? this
              :                                  {};
